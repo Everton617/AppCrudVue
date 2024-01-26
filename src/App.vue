ef3374cb14c5e7837168dcbc2092847d6cef4286
Everton617 <everton6943@gmail.com>
2024-01-26 12:15:58 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <div>
-    <AppHeader title="Teste Crud Laravel"/>
-    <router-view></router-view>
-  </div>
+  <AppHeader title="Teste Crud Laravel"/>
+  <router-view></router-view>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue';
 
+import AppHeader from './components/AppHeader.vue'
 export default {
-  name: 'App',
-  components: {
-    AppHeader
-  },
-  data() {
-    return {
-      connection: null
-    };
-  },
-  created() {
-    console.log("Starting connection to WebSocket Server");
-    this.connection = new WebSocket("wss://53d2-2804-3b38-3b41-b10-a1ab-dbb7-f0ac-f33b.ngrok-free.app");
-
-    this.connection.onmessage = function(event) {
-      console.log(event);
-    };
-
-    this.connection.onopen = function(event) {
-      console.log(event);
-      console.log("Successfully connected to the echo websocket server...");
-    };
-  }
+name: 'App',
+components:{
+  AppHeader
+}
 }
 </script>
 
